e3087324f28b43d07e9346ac327e2d25ee4598d6
unstack-underflow <aronlomn@gmail.com>
2024-01-02 12:04:26 -0500

    Initialized WelcomPage and App setup.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { WelcomePage } from "./pages/WelcomePage";
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<WelcomePage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/WelcomePage.jsx
@@ -0,0 +1,3 @@
+export const WelcomePage = () => {
+  return <div className="font-bold m-[200px]">WelcomePage</div>;
+};
