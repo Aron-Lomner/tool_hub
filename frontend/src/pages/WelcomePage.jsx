aa3829bf88e594c3af3669828916f2379bb2479f
unstack-underflow <aronlomn@gmail.com>
2024-01-02 13:53:00 -0500

    Nothing

--- frontend/src/pages/WelcomePage.jsx
+++ frontend/src/pages/WelcomePage.jsx
@@ -1,3 +1,3 @@
 export const WelcomePage = () => {
-  return <div className="font-bold m-[200px]">WelcomePage</div>;
+  return <div className="font-bold m-[200px] py-0 bg-[#555566] flex flex-row items-center">WelcomePage</div>;
 };
