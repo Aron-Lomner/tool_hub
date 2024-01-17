321374dfc9868a13ba39133af80f7a0aaaa717dc
unstack-underflow <aronlomn@gmail.com>
2024-01-17 09:57:23 -0500

    UserService

---
+++ frontend/src/services/UserService.jsx
@@ -0,0 +1,10 @@
+class UserService {
+  async mockSearchUsersByUsernamePattern() {
+    return [
+      {
+        username: "bob",
+        imageUrl: "https://images.app.goo.gl/tU2BFwxzqYj22RFV8",
+      },
+    ];
+  }
+}
