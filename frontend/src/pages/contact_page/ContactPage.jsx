2a0a3abd274a9de82308805174194bd2ed4f0eef
BroganDR <Brogandunlap@gmail.com>
2024-01-22 10:59:22 -0500

    f

--- frontend/src/pages/contact_page/ContactPage.jsx
+++ frontend/src/pages/contact_page/ContactPage.jsx
@@ -9,7 +9,7 @@ const ContactPage = () => {
         <ContactCard imageUrl="/src/assets/Evan.jpg" description="Columbus, OH" name="Evan Kaczor" linkedIn="https://www.linkedin.com/in/evan-kaczor-777585293/" website="https://ekaczor.github.io/"/>
         <ContactCard imageUrl="/src/assets/emptyImage.webp" description="This is " name="" linkedIn="" website=""/>
         <ContactCard imageUrl="/src/assets/emptyImage.webp" description="Columbus, OH " name="Sebastian Alvarado" linkedIn="https://www.linkedin.com/in/sebastian-alvarado-5091a31b8/" website="https://alealvarado12.github.io/"/>
-        <ContactCard imageUrl="/src/assets/emptyImage.webp" description="This is " name="Brogan Reed" linkedIn="https://www.linkedin.com/in/brogan-reed/" website="https://brogandr.github.io/"/>
+        <ContactCard imageUrl="/src/assets/emptyImage.webp" description="Cleveland, OH " name="Brogan Reed" linkedIn="https://www.linkedin.com/in/brogan-reed/" website="https://brogandr.github.io/"/>
         </div>
         </div>
     )
