e36046e05ebecfab7dfea35de6887ae83fec311c
Sebastian Alvarado <alealvarado1216@gmail.com>
2024-01-22 10:05:52 -0500

    add contact info

--- frontend/src/pages/contact_page/ContactCard.jsx
+++ frontend/src/pages/contact_page/ContactCard.jsx
@@ -6,11 +6,11 @@ const ContactCard = ({imageUrl, description, name, linkedIn, website}) => {
             <img src={imageUrl} alt={name} className="rounded-full w-40 h-40"/>
             <div>
                 <div className="font-bold text-3xl p-2 text-white">{name}</div>
-                <div className="text-lg text-white">{description}</div>
+                <div className="text-lg text-white ">{description}</div>
             </div>
             <div className="flex flex-col ml-auto mt-auto">
-            <a href={linkedIn} className="py-2 px-4 rounded-md transition-transform transform-gpu hover:scale-105 m-5 w-[120px]"><img src="src/assets/Linkedin.png" alt="LinkedIn link" /></a>
-            <a href={website} className="bg-violet-700 text-white py-2 px-4 rounded-md hover:bg-violet-950 transition-transform transform-gpu hover:scale-105 m-5">Website</a>
+            <a href={linkedIn} className="py-2 px-4 rounded-md transition-transform transform-gpu hover:scale-105 m-5 w-[120px]" target="blank"><img src="src/assets/Linkedin.png" alt="LinkedIn link" /></a>
+            <a href={website} className="bg-violet-700 text-white py-2 px-4 rounded-md hover:bg-violet-950 transition-transform transform-gpu hover:scale-105 m-5" target="blank">Website</a>
             </div>
         </div>
     )
